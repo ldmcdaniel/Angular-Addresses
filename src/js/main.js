29847c5ad02e98d16f3051ff7747c08fd670e619
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-06-09 14:08:11 -0500

    working logins and redirects

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 angular
   .module('angularAddresses', ['ngRoute'])
-  .config(function ($routeProvider){
+  //This allows us to use this as a global variable and will do the same as .value()
+  .constant("API_URL", 'https://angular-address-book.firebaseio.com')
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/landing.html'
@@ -11,83 +13,228 @@ angular
       .when('/about', {
         templateUrl: 'views/about.html'
       })
+
       .when('/people', {
         templateUrl: 'views/people.html',
         controller: 'Main',
-        controllerAs: 'main'
+        controllerAs: 'main',
+        private: true
+      })
+      .when('/people/new', {
+        templateUrl: 'views/people.html',
+        controller: 'NewPersonCtrl',
+        controllerAs: 'main',
+        private: true
       })
       .when('/people/:id', {
         templateUrl: 'views/person.html',
         controller: 'PersonController',
-        controllerAs: 'person'
-
-
+        controllerAs: 'main',
+        private: true
       })
-      .otherwise({templateUrl: 'views/404.html'})
+      .when('/people/:id/edit', {
+        templateUrl: 'views/person.html',
+        controller: 'EditPersonCtrl',
+        controllerAs: 'main',
+        private: true
+      })
+
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'auth',
+        resolve: {
+          checkLogin: function ($rootScope, $location) {
+            if ($rootScope.auth) {
+              $location.path('/people')
+            }
+          }
+        }
+      })
+
+      .when('/logout', {
+        template: '<h1>Logging out...</h1>',
+        controller: 'LogoutCtrl'
+      })
+
+      .otherwise({
+        templateUrl: 'views/404.html'
+      });
   })
 
-  .filter('objToArr', function() {
+  .run(function ($rootScope, $location, API_URL) {
+    $rootScope.$on('$routeChangeStart', function (event, nextRoute) {
+      var fb = new Firebase(API_URL);
+      $rootScope.auth = fb.getAuth();
+
+      if (nextRoute.$$route && nextRoute.$$route.private && !$rootScope.auth) {
+        $location.path('/login')
+      }
+    });
+  })
+
+  .filter('objToArr', function () {
     return function (obj) {
       if (obj) {
         return Object
           .keys(obj)
           .map(function (key) {
-            obj[key]['_id'] = key;
+            obj[key]._id = key;
             return obj[key];
           });
       }
     }
   })
 
-  // .filter('ransomcase', function() {
-  //   return function(string) {
-  //     return string
-  //       .split('')
-  //       .map(function(char, i){
-  //         return i % 2 ? char.toUpperCase() : char.toLowerCase();
-  //       })
-  //         .join('')
-  //   }
-  // })
-
-  .controller('PersonController', function ($http, $routeParams) {
-    var vm = this;
-    var id = $routeParams.id;
-
-    $http
-      .get('https://angular-address-book.firebaseio.com/people/' + id + '.json')
-      .success(function (data) {
-        vm.data = data;
-      });
+  .filter('ransomcase', function () {
+    return function (string) {
+      return string
+        .split('')
+        .map(function (char, i) {
+          return i % 2 ? char.toUpperCase() : char.toLowerCase();
+        })
+        .join('');
+    }
   })
-  .controller('Main', function($http) {
+
+  .controller('LogoutCtrl', function ($rootScope, $scope, $location, API_URL) {
+    var fb = new Firebase(API_URL);
+
+    fb.unauth(function () {
+      $rootScope.auth = null;
+      $location.path('/login');
+      $scope.$apply();
+    });
+  })
+
+  .controller('LoginCtrl', function ($rootScope, $scope, $location, API_URL) {
     var vm = this;
 
-    $http
-      .get('https://angular-address-book.firebaseio.com/people.json')
-      .success(function(data){
-        vm.people = data;
+    vm.login = function () {
+      var fb = new Firebase(API_URL);
+
+      fb.authWithPassword({
+        email: vm.email,
+        password: vm.password
+      }, function (err, authData) {
+        if (err) {
+          console.log('Error', err)
+        } else {
+          $rootScope.auth = authData;
+          $location.path('/people');
+          $scope.$apply();
+        }
       });
 
-  vm.newPerson = {}
+    };
 
-  vm.addNewAddress = function() {
-    $http
-      .post('https://angular-address-book.firebaseio.com/people.json', vm.newPerson)
-      .success(function (res) {
-        vm.people[res.name] = vm.newPerson;
-        //resetting the form here unlinks the input from the
-        //table entry which would be unneeded on a submit to a new page
-        vm.newPerson = {};
-      });
-  };
+    vm.register = function () {};
+  })
 
-  vm.removeAddress = function(id) {
-    console.log(id);
-    $http
-      .delete('https://angular-address-book.firebaseio.com/people/' + id + '.json')
-      .success(function() {
-        delete vm.people[id]
+  .controller('PersonController', function ($routeParams, $location, Person) {
+    var vm = this;
+    vm.id = $routeParams.id;
+
+    Person.getOne(vm.id, function (data) {
+      vm.person = data;
+    });
+
+    vm.destroy = function (id) {
+      Person.destroy(vm.id, function () {
+        $location.path('/people');
       });
-  };
-});
+    };
+
+    vm.onModalLoad = function () {};
+  })
+
+  .factory('Person', function ($http, API_URL) {
+    return {
+      getOne(id, cb) {
+        $http
+          .get(`${API_URL}/people/${id}.json`)
+          .success(cb);
+      },
+
+      getAll(cb) {
+        $http
+          .get(`${API_URL}/people.json`)
+          .success(cb);
+      },
+
+      create(data, cb) {
+        $http
+          .post(`${API_URL}/people.json`, data)
+          .success(cb);
+      },
+
+      update(id, data, cb) {
+        $http
+          .put(`${API_URL}/people/${id}.json`, data)
+          .success(cb);
+      },
+
+      destroy(id, cb) {
+        $http
+          .delete(`${API_URL}/people/${id}.json`)
+          .success(cb);
+      }
+    }
+  })
+
+  .controller('NewPersonCtrl', function ($location, $scope, Person) {
+    var vm = this;
+
+    vm.onModalLoad = function () {
+      $('#modal').modal('show');
+
+      $('#modal').on('hidden.bs.modal', function (e) {
+        $location.path('/people');
+        $scope.$apply();
+      });
+    };
+
+    vm.saveAddress = function () {
+      Person.create(vm.person, function () {
+        $('#modal').modal('hide');
+      });
+    };
+
+    Person.getAll(function (people) {
+      vm.people = people;
+    });
+  })
+
+  .controller('EditPersonCtrl', function ($scope, $routeParams, $location, Person) {
+    var vm = this;
+    vm.id = $routeParams.id;
+
+    vm.onModalLoad = function () {
+      $('#modal').modal('show');
+
+      $('#modal').on('hidden.bs.modal', function (e) {
+        $location.path(`/people/${vm.id}`);
+        $scope.$apply();
+      });
+    };
+
+    vm.saveAddress = function () {
+      Person.update(vm.id, vm.person, function () {
+        $('#modal').modal('hide');
+      });
+    };
+
+    Person.getOne(vm.id, function (person) {
+      vm.person = person;
+    });
+  })
+
+  .controller('Main', function ($rootScope, $location, Person) {
+    var vm = this;
+
+    Person.getAll(function (people) {
+      vm.people = people;
+    });
+
+    vm.onModalLoad = function () {};
+  });
